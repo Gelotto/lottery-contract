0ac94676e5186c937510d0c76c8087a0619da6eb
Daniel Gabriele <dg0offset@gmail.com>
2022-09-05 17:56:45 -0400

    fixed address

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,3 @@
 pub const GELOTTO_ADDR: &str = "juno1jume25ttjlcaqqjzjjqx9humvze3vcc8z87szj";
-pub const GELOTTO_ANNUAL_GRAND_PRIZE_ADDR: &str = "juno1jume25ttjlcaqqjzjjqx9humvze3vcc8z87szj";
+pub const GELOTTO_ANNUAL_GRAND_PRIZE_ADDR: &str = "juno1fxu5as8z5qxdulujzph3rm6c39r8427mjnx99r";
 pub const GELOTTO_SHOPPE_REWARDS_ADDR: &str = "juno1tlyqv2ss4p9zelllxm39hq5g6zw384mvvym6tp";
